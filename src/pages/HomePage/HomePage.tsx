b790574a059cbc2be44775404adeb664c71e0275
daniskhabirov <dakhabirov@gmail.com>
2023-08-25 12:04:33 +0300

    fix: add search history item from home page.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,13 +3,16 @@ import { useNavigate } from "react-router-dom";
 
 import SearchForm from "../../components/SearchForm/SearchForm";
 import Form from "../../components/SearchForm/Form";
+import useHistory from "../../hooks/useHistory";
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const { addHistoryItem } = useHistory();
 
   const form = Form();
 
   const handleSubmit = () => {
+    addHistoryItem({ searchValue: form.values });
     navigate(
       `/search?search=${form.values.search}&year=${form.values.year}&type=${form.values.type}`,
     );
