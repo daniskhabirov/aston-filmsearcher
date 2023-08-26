e25968499509cf5802cc62e89d973fef3681609a
daniskhabirov <dakhabirov@gmail.com>
2023-08-26 09:01:34 +0300

    Refactor.

--- src/pages/HistoryPage/HistoryPage.tsx
+++ src/pages/HistoryPage/HistoryPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ActionIcon, Grid, Table } from "@mantine/core";
+import { ActionIcon, Stack, Table, Text } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
 
 import { IconTrash } from "@tabler/icons-react";
@@ -22,50 +22,49 @@ const HistoryPage = () => {
   };
 
   return (
-    <Grid>
-      <Grid.Col>
-        <Table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Search</th>
-              <th>Year</th>
-              <th>Type</th>
-              <th>Date</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {historyItems.map((element) => (
-              <tr
-                key={element.date}
-                onClick={() => {
-                  navigate(
-                    `/search?search=${element.search}&year=${element.year}&type=${element.type}`,
-                  );
-                }}
+    <Stack>
+      <Text align="center">Search history</Text>
+      <Table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Search</th>
+            <th>Year</th>
+            <th>Type</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {historyItems.map((element) => (
+            <tr
+              key={element.date}
+              onClick={() => {
+                navigate(
+                  `/search?search=${element.search}&year=${element.year}&type=${element.type}`,
+                );
+              }}
+            >
+              <td style={{ width: "60%" }}>{element.search}</td>
+              <td style={{ width: "10%" }}>{element.year}</td>
+              <td style={{ width: "10%" }}>{element.type}</td>
+              <td style={{ width: "15%" }}>{element.date}</td>
+              <td
+                align="center"
+                style={{ width: "5%", cursor: "default" }}
+                onClick={(e) => e.stopPropagation()}
               >
-                <td style={{ width: "60%" }}>{element.search}</td>
-                <td style={{ width: "10%" }}>{element.year}</td>
-                <td style={{ width: "10%" }}>{element.type}</td>
-                <td style={{ width: "15%" }}>{element.date}</td>
-                <td
-                  align="center"
-                  style={{ width: "5%", cursor: "default" }}
-                  onClick={(e) => e.stopPropagation()}
+                <ActionIcon
+                  onClick={() => deleteHistoryClick(element.id)}
+                  className={styles.icon}
                 >
-                  <ActionIcon
-                    onClick={() => deleteHistoryClick(element.id)}
-                    className={styles.icon}
-                  >
-                    <IconTrash />
-                  </ActionIcon>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Grid.Col>
-    </Grid>
+                  <IconTrash />
+                </ActionIcon>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Stack>
   );
 };
 
