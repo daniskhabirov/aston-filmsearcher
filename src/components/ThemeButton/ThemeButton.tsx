5c4abff2d0343203e2ca055a3a7b9bef3f78123b
daniskhabirov <dakhabirov@gmail.com>
2023-09-06 23:55:58 +0300

    feat: cursor pointer at theme button

--- src/components/ThemeButton/ThemeButton.tsx
+++ src/components/ThemeButton/ThemeButton.tsx
@@ -1,19 +1,20 @@
 import React, { useContext } from "react";
 import { Switch } from "@mantine/core";
 
-import { IconBrightnessDown, IconMoon } from "@tabler/icons-react";
+import { IconSun, IconMoonStars } from "@tabler/icons-react";
 
 import { ThemeContext } from "../../utils/themeContext";
 
 const ThemeButton = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
+
   return (
     <Switch
       checked={theme === "dark"}
       onChange={toggleTheme}
       size="md"
-      onLabel={<IconBrightnessDown width={16} height={16} />}
-      offLabel={<IconMoon width={16} height={16} />}
+      onLabel={<IconSun width={16} height={16} cursor="pointer" />}
+      offLabel={<IconMoonStars width={16} height={16} cursor="pointer" />}
     />
   );
 };
