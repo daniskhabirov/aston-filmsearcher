f14235d1a5fda5dcbd6447e41d57b480608a3c07
daniskhabirov <dakhabirov@gmail.com>
2023-09-06 22:50:44 +0300

    refactor

--- src/app/reducers/selectors.ts
+++ src/app/reducers/selectors.ts
@@ -9,7 +9,6 @@ export const getUserName = (state: RootState): string => state.user.userName;
 
 export const getHistoryItems = (state: RootState): HistoryItem[] =>
   state.user.historyItems;
-
 export const getFavoriteCardIds = (state: RootState): string[] =>
   state.user.favoriteCardIds;
 export const getFavoriteCards = (state: RootState): Card[] =>
